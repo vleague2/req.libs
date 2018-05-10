88c0cb146614362dca362f094b5ab800a2f73532
BenjaminJVaccaro <benvaccaro14@gmail.com>
2018-05-10 19:55:29 -0400

    Ignore and delete this

--- models/user.js
+++ models/user.js
@@ -1,22 +1,47 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        is: ["^[a-z]+$",'i']
+        is: ["^[a-z]+$", 'i']
       }
     },
     lastName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        is: ["^[a-z]+$",'i']
+        is: ["^[a-z]+$", 'i']
       }
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    hash: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    activationKey: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    resetPasswordKey: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    verified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
     }
   });
   return User;
