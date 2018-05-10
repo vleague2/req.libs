85209bb23b8aa2d503286d266dbdeb66162b7b27
Alex Trilling <atrill90@gmail.com>
2018-05-10 14:44:50 -0400

    adding routes for google login

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,13 @@
-
+//authenticating google sign in
+app.post('/login', passport.authenticate('local', { successRedirect: '/',failureRedirect: '/login' }));
    
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
   
 
 
