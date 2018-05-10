3f79c286a2d0c39bcb4329c1b20550498532a50d
unknown <vleague2@gmail.com>
2018-05-10 11:29:08 -0400

    removed duplicate folder

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Req.Libs</a>
+</nav>
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
