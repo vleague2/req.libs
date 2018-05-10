a32847833a4b84b0539860fae41f70454e3c7cf6
unknown <vleague2@gmail.com>
2018-05-10 11:31:47 -0400

    fixed index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Navbar from '@/components/Navbar'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Navbar
+      component: HelloWorld
     }
   ]
 })
