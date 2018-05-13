9910ebb71e103dcc96e4a2ea88b3476bf9f599c3
BenjaminJVaccaro <benvaccaro14@gmail.com>
2018-05-13 16:20:50 -0400

    it now console logs out the story with the user inputs

--- controllers/stories-api-routes.js
+++ controllers/stories-api-routes.js
@@ -21,12 +21,14 @@ router.get("/", function(req, res) {
 
 // getting page for user to select which story they want
 router.get("/category/:id", (req, res) => {
+    let query = req.params.id;
     console.log("fetching objects..");
-
+    query = query.replace("%20", " ");
+  console.log(query);
   // sequelize to query the stories for the drop down menu
   db.Story.findAll({
     where: {
-      category: req.params.id
+      category: query
     }
   }).then(data => {
     console.log("findAll...");
@@ -41,7 +43,7 @@ router.get("/category/:id", (req, res) => {
 });
 
 // getting page for user to input blanks based on story they selected
-router.get("/user-input/:id", (req, res) => {
+router.get("/userInput/:id", (req, res) => {
   console.log("fetching objects..");
 
   // sequelize to query the blank inputs for the user to fill in
@@ -56,26 +58,69 @@ router.get("/user-input/:id", (req, res) => {
     // rendering inputs page
     res.render("inputs", {
 
-      // passing in object blanks with data      
+      // passing in object blanks with data    
+    storiesId: req.params.id,  
     blanks: data
     });
+    // res.json(data);
   })
 });
 
 // getting page for user to read story they created
-router.post("/user-create-story/", function(req, res) {
+router.post("/userStory/:id", function(req, res) {
   console.log("posting...");
+  db.Story.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    console.log("creating story..");
+    let storyBody = data.body;
+    console.log(storyBody);
 
-  // looping over the user's inputs
-  for (i=0;i<req.body.userInput.length;i++) {
+    for (let i = 0; i < req.body.userInput.length; i++) {
+        storyBody = storyBody.replace(/(\*)(\*)(\*)/, function(match, p1) {
+           return req.body.userInput[i];
+          console.log(storyBody);
+           
+        });
+    console.log(storyBody);
+    };
 
-    // pushing the users's inputs to the db under the userInputs table
-    db.UserInput.create({
-      userInput: req.body.userInput[i]
-    }).then(newUserInput => {
-      console.log(`Added successfully`);
-  });
-}
+    // rendering category
+    // res.render("category", {
+      // passing in object "stories" with data
+    //   stories: data
+    // });
+  })
+  // console.log(req.body.userInput);
+  // // looping over the user's inputs
+  // for (i=0;i<req.body.userInput.length;i++) {
+
+  //   // pushing the users's inputs to the db under the userInputs table
+  //   db.UserInput.create({
+  //     userInput: req.body.userInput[i]
+  //   }).then(newUserInput => {
+  //     console.log(`Added successfully`);
+  //   });
+  // }
+
+  // db.UserInput.findAll({
+  //   include: [{
+  //     model: db.Story,
+  //     where: {id: req.params.id}
+  // }]
+  // }).then(data => {
+  //   console.log("findAll...");
+
+  //   // rendering inputs page
+  //   res.render("inputs", {
+
+  //     // passing in object blanks with data      
+  //   blanks: data
+  //   });
+  // })
+
 });
 
 
