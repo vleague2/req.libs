25cee1fcc6f0096c3de2ddcd5998d9d4215be5e7
Trey <treyw1850@gmail.com>
2018-05-14 13:21:43 -0400

    working on more js

--- public/js/user-created.js
+++ public/js/user-created.js
@@ -2,20 +2,25 @@ console.log('im working')
 
 let partOfSpeechArr = [];
 
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("add-speech-part").addEventListener('click',function ()
     {
 
+        let wholeStory = document.getElementById('add-story-parts');
+        var newSpeech = document.getElementById('catch-speech').value;
        
-        var newSpeech =  document.getElementById('catch-speech').value;
-        console.log(newSpeech);
-
-        // document.getElementById('catch-speech').value - ''
+        // console.log(newSpeech);
 
 
-        partOfSpeechArr.push(newSpeech)
-     console.log('hello');
+        console.log(wholeStory.value);
+        wholeStory = wholeStory.value + ' >>' + newSpeech + '<< ';
+        console.log(wholeStory);
+
+        partOfSpeechArr.push(newSpeech);
      console.log(partOfSpeechArr);
+     $("#catch-speech").val('');
+     
      
     }  ); 
 });
